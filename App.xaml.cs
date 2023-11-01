109bc885dd3929f6a75825b30bb7c1abc6ec33a9
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-10-31 18:17:18 -0700

    Initial Commit

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Prog_2_Classes_Cont;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
